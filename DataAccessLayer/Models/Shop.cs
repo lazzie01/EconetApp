1e075cdcfe109acada3a43f0ab918b2dff50315b
lazarus.munetsi <lazarus.munetsi@innroad.com>
2022-04-28 09:55:06 +0200

    project initialization

---
+++ DataAccessLayer/Models/Shop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Models
+{
+    public class Shop
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
