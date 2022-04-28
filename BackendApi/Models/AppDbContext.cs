fd6402c000f0639a91be3379471d7f44ae3855c4
lazarus.munetsi <lazarus.munetsi@innroad.com>
2022-04-28 10:06:47 +0200

    restructured the project

---
+++ BackendApi/Models/AppDbContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace BackendApi.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Shop> Shops { get; set; }
+        public DbSet<Area> Areas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Seed Department Table
+            modelBuilder.Entity<Area>().HasData(new Area { Id = 1, Name = "Harare CBD" });
+            modelBuilder.Entity<Area>().HasData(new Area { Id = 2, Name = "Bulawayo CBD" });
+            modelBuilder.Entity<Area>().HasData(new Area { Id = 3, Name = "Mutare CBD" });
+
+            //Seed Employee Table
+            modelBuilder.Entity<Shop>().HasData(new Shop
+            {
+                Id = 1,
+                Name = "Econet First Street",
+                AreaId = 1
+            });
+
+            modelBuilder.Entity<Shop>().HasData(new Shop
+            {
+                Id = 2,
+                Name = "Econet Joina",
+                AreaId = 1
+            });
+
+            modelBuilder.Entity<Shop>().HasData(new Shop
+            {
+                Id = 3,
+                Name = "Econet Bulawayo",
+                AreaId = 2
+            });
+        }
+    }
+}
---
+++ BackendApi/Models/Shop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackendApi.Models
+{
+    public class Shop
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int AreaId { get; set; }
+
+        public virtual Area Area { get; set; }
+    }
+}
