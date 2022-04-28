756d5fdf391c55e6e23f3810614bc417237da58a
lazarus.munetsi <lazarus.munetsi@innroad.com>
2022-04-28 10:45:52 +0200

    added area and shop controllers

---
+++ BackendApi/Controllers/AreaController.cs
@@ -0,0 +1,38 @@
+﻿using BackendApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendApi.Controllers
+{
+    public class AreaController : ControllerBase
+    {
+        private readonly IAreaRepository areaRepository;
+
+        public AreaController(IAreaRepository areaRepository)
+        {
+            this.areaRepository = areaRepository;
+        }
+
+        [HttpGet("{search}")]
+        public async Task<ActionResult<IEnumerable<Shop>>> Search(string name)
+        {
+            try
+            {
+                var result = await areaRepository.Search(name);
+
+                if (result.Any())
+                    return Ok(result);
+                return NotFound();
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database");
+            }
+        }
+    }
+}
---
+++ BackendApi/Controllers/ShopController.cs
@@ -0,0 +1,62 @@
+﻿using BackendApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace BackendApi.Controllers
+{
+    public class ShopController : ControllerBase
+    {
+        private readonly IShopRepository shopRepository;
+
+        public ShopController(IShopRepository shopRepository)
+        {
+            this.shopRepository = shopRepository;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Shop>> CreateShop(Shop shop)
+        {
+            try
+            {
+                if (shop == null)
+                    return BadRequest();
+
+                var existingShop = await shopRepository.GetShopByName(shop.Name);
+
+                if (existingShop != null)
+                {
+                    ModelState.AddModelError("Name", "Shop name already in use");
+                    return BadRequest(ModelState);
+                }
+
+                var createdShop = await shopRepository.AddShop(shop);
+
+                return CreatedAtAction(nameof(GetShop), new { id = createdShop.Id }, createdShop);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error creating new employee record");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Shop>> GetShop(int id)
+        {
+            try
+            {
+                var result = await shopRepository.GetShop(id);
+                if (result == null)
+                    return NotFound();
+                return result;
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database");
+            }
+        }
+    }
+}
